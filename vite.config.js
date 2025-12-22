6ad4b72bcdca0fc95ed09939608cc1e2bffffd69
sampaiofree <sampaio.free@gmail.com>
2025-12-22 11:38:38 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/edux-base.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
